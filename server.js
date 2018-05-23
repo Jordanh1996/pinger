94c9d90aa75ed37d33ce5832b294a3d8cd0b42a2
Jordan Hury <jordan@Jordans-MacBook-Pro.local>
2018-05-23 17:53:58 +0300

    now it pings self

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ setInterval(() => {
     http.get("https://expensifyserver.herokuapp.com/");
     http.get("https://blog-jordan.herokuapp.com/");
     http.get("https://blogserver-jordan.herokuapp.com/");
-
+    http.get("https://pinger-jordan/");
 }, 300000); // every 5 minutes (300000)
