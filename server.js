91043be4ff5949bf7574489abc0c147d67eb8c53
Jordan Hury <jordan@Jordans-MacBook-Pro.local>
2018-05-23 17:51:38 +0300

    pinger

---
+++ server.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+setInterval(() => {
+    http.get("https://expensify-jordan.herokuapp.com/");
+    http.get("https://expensifyserver.herokuapp.com/");
+    http.get("https://blog-jordan.herokuapp.com/");
+    http.get("https://blogserver-jordan.herokuapp.com/");
+
+}, 300000); // every 5 minutes (300000)
