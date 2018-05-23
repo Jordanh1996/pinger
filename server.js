280e8d5ac42f35b24be468b0112ff9f1c2898bc1
Jordan Hury <jordan@Jordans-MacBook-Pro.local>
2018-05-23 18:49:09 +0300

    binded to port|

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
+const express = require('express');
 const http = require('http');
+
+const app = express();
+port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('this app is awake');
+});
 
 setInterval(() => {
     http.get("http://expensify-jordan.herokuapp.com/");
@@ -7,3 +15,7 @@ setInterval(() => {
     http.get("http://blogserver-jordan.herokuapp.com/");
     http.get("http://pinger-jordan/");
 }, 300000); // every 5 minutes (300000)
+
+app.listen(port, () => {
+    console.log(`server is up on port ${port}`);
+});
